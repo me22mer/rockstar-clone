0d7bc1a00e157ab0943cd107c15be9ab6d62d9c4
Y3sterd4y <nongboy.chaiwat@gmail.com>
2024-02-14 00:06:19 +0700

    Update header

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,21 +1,21 @@
-import { twMerge } from "tailwind-merge";
-
+import { cn } from "../lib/utils";
 type Props = {
-  className: string;
-  children: React.ReactNode;
-};
+  className?: string;
+  children?: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export default function Button({ className, children }: Props) {
+export default function Button({ className, children, ...props }: Props) {
   return (
     <button
-      className={twMerge(
+      className={cn(
         "px-5 py-8 relative text-zinc-200 font-medium group hover:text-white",
         className
       )}
+      {...props}
     >
       {children}
       <span
-        className={twMerge(
+        className={cn(
           "absolute bottom-0 left-0 w-full h-1 bg-transparent border-b-2 border-transparent group-hover:border-zinc-200 duration-300"
         )}
       ></span>
---
+++ src/app/icons/global.tsx
@@ -0,0 +1,20 @@
+export default function GlobalIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="" fill="none">
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10ZM2 12h20"
+      />
+      <path
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10v0Z"
+      />
+    </svg>
+  );
+}
